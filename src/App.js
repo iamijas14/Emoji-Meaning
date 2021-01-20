a815c8e52d1bf8c4a249a612647a21e67d9e245a
Ijas mohammed <75926327+iamijas14@users.noreply.github.com>
2021-01-20 13:52:25 +0530

    feat:updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+var EmojisDictionary = {
+  "⚽": "Soccer Ball",
+  "⚾": "Baseball",
+  "🥎": "Softball",
+  "🏀": "Basketball",
+  "🏐": "Volleyball",
+  "🏈": "American Football",
+  "🏉": "Rugby Football",
+  "🎾": "Tennis"
+};
+
+var emojiweknow = Object.keys(EmojisDictionary);
 
 export default function App() {
+  var [emoji, setemoji] = useState();
+  function inputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = EmojisDictionary[userInput];
+    setemoji(meaning);
+  }
+  function clickHandler(emoji) {
+    var meaning = EmojisDictionary[emoji];
+    setemoji(meaning);
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Emojis meaning.</h1>
+      <input
+        id="input"
+        onChange={inputHandler}
+        placeholder="enter the emoji here"
+      ></input>
+      <h2>{emoji}</h2>
+      <h3>emoji's we know:</h3>
+      <div>
+        {emojiweknow.map(function (emoji) {
+          return (
+            <span
+              key={emoji}
+              style={{
+                padding: "0.5rem",
+                fontSize: "1.5rem",
+                cursor: "pointer"
+              }}
+              onClick={() => clickHandler(emoji)}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
